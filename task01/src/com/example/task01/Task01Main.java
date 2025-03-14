2d5477d6c8b48a6520842e0e1b3d539bc24824a9
lenadekart <decartfamily@vk.com>
2025-03-14 21:23:33 +0500

    Симоненко Леонид. Задачи 8.1 - 8.3.

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,19 +2,33 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath())
+                .redirectOutput(ProcessBuilder.Redirect.PIPE);
+        Process process = processBuilder.start();
+
+        String title = "format.tags.title";
+        String soundName = "";
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))){
+            soundName = reader.lines()
+                    .filter(p -> p.contains(title))
+                    .findFirst()
+                    .map(x -> x.substring(title.length() + 2, x.length() - 1))
+                    .orElse(""); // Используйте orElse для предотвращения NoSuchElementException
+        }
+        return soundName;
     }
 }
